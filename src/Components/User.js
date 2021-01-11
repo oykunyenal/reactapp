f334f1fbaaed4a3c021c6f907acb63deebab8c59
yenal.oykun@outlook.com <yenal.oykun@outlook.com>
2021-01-11 19:59:56 +0300

    commit

---
+++ src/Components/User.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class User extends Component {
+    render() {
+
+        //Destruction 
+        const {name,department,salary} = this.props;
+
+        return (
+            
+            <div className = "col-md-8 mb-4">
+             <div className = "card">
+                <div className="card-header d-flex justify-content-between">
+                    <h4 className = "d-inline">{name}</h4>
+                    <i className = "far fa-trash-alt" style = {{ cursor : "pointer"}}></i>
+                </div>
+                <div className="card-body">
+                <p className="card-text"> Maaş : {salary}</p>
+                <p className="card-text"> Bölüm : {department}</p>
+                </div>
+             </div>
+            </div>
+        )
+    }
+}
+
+User.defaultProps = {
+    name : "Bilgi Yok",
+    salary : "Bilgi Yok",
+    department : "Bilgi Yok"
+}
+
+User.propTypes = {
+    name : PropTypes.string.isRequired,
+    salary : PropTypes.string.isRequired,
+    department : PropTypes.string.isRequired
+}
+
+
+
+export default User;
